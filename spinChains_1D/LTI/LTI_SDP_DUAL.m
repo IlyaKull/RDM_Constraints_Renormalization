3d68796a3c78f76128492dd935cd75bdb150a085
IlyaKull <ilyakull@gmail.com>
2024-04-22 10:36:53 +0100

    added some documentation

--- spinChains_1D/LTI/LTI_SDP_DUAL.m
+++ spinChains_1D/LTI/LTI_SDP_DUAL.m
@@ -1,6 +1,7 @@
 function [ ElocTIRig,ElocTI] = LTI_SDP_DUAL(N,H,sdpOps)
 %solve the locally translation invariant problem for a state of N spins
 
+% support different input formats for Hamiltonian term H
 if isstruct(H)
     suppH=H.suppH; % the number of sites H acts on 
     d = H.d; % the dimention of each site
@@ -13,7 +14,7 @@ end
  
 alpha=sdpvar(d^(N-1)); %first state is on k0+1 sites. Alpha is the local transl. inv. dual variable
 
-sdpvar epsil
+sdpvar epsil % dual variable of the tr(rho)==1 constraint
 
 expr = kron(H,speye(d^(N-suppH))) ...
     + dontUseKron(alpha,d^(N-1),d,'IX') ...
@@ -29,7 +30,9 @@ objectiveFunc = - epsil; %minus sign for maximization
 ElocTI=-value(objectiveFunc);
  
 expr = value(expr);
+
+% we can obtain a certified result by making sure that the dual variables obtained from the solver are feasible (to machine precision)
 minEig = min(real(eig( expr )));
- 
+% the corrected energy value is then:
 ElocTIRig=value(epsil) + minEig;
  
