1524360a9ba1c444ad89f8cea0d6d61ff4c2b9e0
IlyaKull <ilyakull@gmail.com>
2024-04-22 10:15:13 +0100

    folder removed from master

--- spinChains_1D/MPS_Relaxation_with_Gradient/declarePrimalVars.m
+++
@@ -1,6 +0,0 @@
-function [rho,omega]=declarePrimalVars(n,d,D,k0) %(n,D,k0,H,CGmaps, sdpOps)
-omega=cell(n,1);
-rho=sdpvar(d*d^(k0-1)*d,d*d^(k0-1)*d,'symmetric');
-for j=k0:n
-    omega{j}=sdpvar(d*D*D*d,d*D*D*d,'symmetric');
-end
